00a02362637e44c73ef1537b52497471f89083f9
Garret Rieger <grieger@google.com>
2021-08-16 15:45:07 -0700

    Update base 128 decode/encode to match the specified implementation.
    Notably the encoding order of the bytes has been reversed to be big endian.

--- patch_subset/cbor/int_utils.cc
+++ patch_subset/cbor/int_utils.cc
@@ -26,79 +26,68 @@ int32_t IntUtils::ZigZagDecode(uint32_t unsigned_int) {
   }
 }
 
-// Perhaps pass in vector<> and resize as needed?
-StatusCode IntUtils::UIntBase128Encode(uint32_t unsigned_int, uint8_t* buffer,
+StatusCode IntUtils::UIntBase128Encode(uint32_t value, uint8_t* buffer,
                                        size_t* size_in_out) {
   if (buffer == nullptr || size_in_out == nullptr || *size_in_out <= 0) {
     return StatusCode::kInvalidArgument;
   }
-  size_t num_bytes = 0;
-  size_t size = *size_in_out;
-  bool more_bytes;
-  do {
-    if (size == 0) {
-      return StatusCode::kInvalidArgument;
+
+  size_t size = IntUtils::UintBase128EncodedSize(value);
+  if (*size_in_out < size) {
+    return StatusCode::kInvalidArgument;
+  }
+
+  for (size_t i = 0; i < size; ++i) {
+    int b = static_cast<int>((value >> (7 * (size - i - 1))) & 0x7f);
+    if (i < size - 1) {
+      b |= 0x80;
     }
-    uint8_t last_7_bits = unsigned_int & 0b01111111;
-    unsigned_int >>= 7;
-    more_bytes = unsigned_int != 0;
-    if (more_bytes) {
-      last_7_bits |= 0b10000000;  // Signal more bytes to come.
-    }
-    *buffer++ = last_7_bits;
-    size--;
-    num_bytes++;
-  } while (more_bytes);
-  *size_in_out = num_bytes;
+    buffer[i] = b;
+  }
+  *size_in_out = size;
   return StatusCode::kOk;
 }
 
 StatusCode IntUtils::UintBase128Decode(string_view bytes, uint32_t* uint_out,
                                        size_t* num_bytes_out) {
-  if (bytes.empty() || uint_out == nullptr || num_bytes_out == nullptr) {
+  if (bytes.data() == nullptr || bytes.empty() || uint_out == nullptr || num_bytes_out == nullptr) {
     return StatusCode::kInvalidArgument;
   }
-  const char* buffer = bytes.data();
-  if (buffer == nullptr) {
-    return StatusCode::kInvalidArgument;
-  }
-  size_t size = bytes.size();
-  size_t num_bytes = 0;
+
   uint32_t result = 0;
-  bool high_bit;
-  do {
-    if (num_bytes >= 5) {
-      // At most 5 bytes are used. Don't read any more.
+  unsigned i = 0;
+  for (const auto c : absl::ClippedSubstr(bytes, 0, 5)) {
+    // No leading 0’s
+    if (i == 0 && c == 0x80) {
       return StatusCode::kInvalidArgument;
     }
-    if (size == 0) {
-      // Ran out of bytes to read.
+
+    // If any of the top seven bits are set then we're about to overflow.
+    if (result & 0xFE000000) {
       return StatusCode::kInvalidArgument;
     }
-    uint8_t next_7_bits = *buffer++;
-    size--;
-    num_bytes++;
-    high_bit = next_7_bits & 0b10000000;
-    next_7_bits &= 0b01111111;
-    int shift = 7 * ((int)num_bytes - 1);
-    if (shift == 5 && next_7_bits & 0b1110000) {
-      // Shifting bits would overflow uint_32.
-      return StatusCode::kInvalidArgument;
+
+    result = (result << 7) | (c & 0x7f);
+
+    // Spin until most significant bit of data byte is false
+    if ((c & 0x80) == 0) {
+      *uint_out = result;
+      *num_bytes_out = i + 1;
+      return StatusCode::kOk;
     }
-    result = (next_7_bits << shift) | result;
-  } while (high_bit);
-  *uint_out = result;
-  *num_bytes_out = num_bytes;
-  return StatusCode::kOk;
+
+    i++;
+  }
+
+  return StatusCode::kInvalidArgument;
 }
 
-int IntUtils::UintBase128EncodedSize(uint32_t uint) {
-  int num_bytes = 0;
-  do {
-    uint >>= 7;
-    num_bytes++;
-  } while (uint);
-  return num_bytes;
+int IntUtils::UintBase128EncodedSize(uint32_t value) {
+  int size = 1;
+  for (; value >= 128; value >>= 7) {
+    ++size;
+  }
+  return size;
 }
 
 }  // namespace patch_subset::cbor
