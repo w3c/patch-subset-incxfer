529acbdfe1794d7e2b28532ad21ebb4af9bcbd69
Andy John <andyj@google.com>
2021-09-08 16:29:15 -0700

    Add second checksum, rename fields in ClientState.

--- patch_subset/cbor/client_state.h
+++ patch_subset/cbor/client_state.h
@@ -20,21 +20,24 @@ class ClientState {
  private:
   std::optional<std::string> _font_id;
   std::optional<std::string> _font_data;
-  std::optional<uint64_t> _fingerprint;
+  std::optional<uint64_t> _original_font_checksum;
   std::optional<std::vector<int32_t>> _codepoint_remapping;
+  std::optional<uint64_t> _codepoint_remapping_checksum;
 
   static const int kFontIdFieldNumber = 0;
   static const int kFontDataFieldNumber = 1;
-  static const int kFingerprintFieldNumber = 2;
+  static const int kOriginalFontChecksumFieldNumber = 2;
   static const int kCodepointRemappingFieldNumber = 3;
+  static const int kCodepointRemappingChecksumFieldNumber = 4;
 
  public:
   ClientState();
   ClientState(const ClientState& other) = default;
   ClientState(ClientState&& other) noexcept;
   ClientState(const std::string& font_id, const std::string& font_data,
-              uint64_t fingerprint,
-              const std::vector<int32_t>& codepoint_remapping);
+              uint64_t original_font_checksum,
+              const std::vector<int32_t>& codepoint_remapping,
+              uint64_t codepoint_remapping_checksum);
 
   static StatusCode Decode(const cbor_item_t& cbor_map, ClientState& out);
   StatusCode Encode(cbor_item_unique_ptr& out) const;
@@ -49,16 +52,21 @@ class ClientState {
   [[nodiscard]] bool HasFontData() const;
   [[nodiscard]] const std::string& FontData() const;
 
-  ClientState& SetFingerprint(uint64_t);
-  ClientState& ResetFingerprint();
-  [[nodiscard]] bool HasFingerprint() const;
-  [[nodiscard]] uint64_t Fingerprint() const;
+  ClientState& SetOriginalFontChecksum(uint64_t);
+  ClientState& ResetOriginalFontChecksum();
+  [[nodiscard]] bool HasOriginalFontChecksum() const;
+  [[nodiscard]] uint64_t OriginalFontChecksum() const;
 
   ClientState& SetCodepointRemapping(
       const std::vector<int32_t>& codepoint_remapping);
   ClientState& ResetCodepointRemapping();
   [[nodiscard]] bool HasCodepointRemapping() const;
   [[nodiscard]] const std::vector<int32_t>& CodepointRemapping() const;
+
+  ClientState& SetCodepointRemappingChecksum(uint64_t);
+  ClientState& ResetCodepointRemappingChecksum();
+  [[nodiscard]] bool HasCodepointRemappingChecksum() const;
+  [[nodiscard]] uint64_t CodepointRemappingChecksum() const;
 
   ClientState& operator=(ClientState&& other) noexcept;
   bool operator==(const ClientState& other) const;
