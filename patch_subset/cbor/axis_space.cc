87584c6b073e7a1117abea35a556f10165861cb3
Garret Rieger <grieger@google.com>
2022-04-07 16:22:58 -0700

    Add AxisSpace implementation.

---
+++ patch_subset/cbor/axis_space.cc
@@ -0,0 +1,121 @@
+#include "patch_subset/cbor/axis_space.h"
+
+#include "patch_subset/cbor/cbor_utils.h"
+
+namespace patch_subset::cbor {
+
+bool AxisSpace::Has(hb_tag_t tag) const {
+  return _space.contains(tag);
+}
+
+void AxisSpace::Clear(hb_tag_t tag) {
+  if (auto it = _space.find(tag); it != _space.end()) {
+    it->second.clear();
+  }
+}
+
+void AxisSpace::AddInterval(hb_tag_t tag, const AxisInterval& interval) {
+  _space[tag].push_back(interval);
+}
+
+const std::vector<AxisInterval>& AxisSpace::IntervalsFor(hb_tag_t tag) const {
+  static std::vector<AxisInterval> empty;
+
+  if (auto it = _space.find(tag); it != _space.end()) {
+    return it->second;
+  }
+
+  return empty;
+}
+
+StatusCode AxisSpace::Decode(const cbor_item_t& cbor_map, AxisSpace& out) {
+  if (!cbor_isa_map(&cbor_map)) {
+    return StatusCode::kInvalidArgument;
+  }
+
+  StatusCode sc;
+  AxisSpace result;
+
+  size_t map_size = cbor_map_size(&cbor_map);
+  cbor_pair* pairs = cbor_map_handle(&cbor_map);
+  for (unsigned i = 0; i < map_size; i++) {
+
+    cbor_item_t* tag_str = pairs->key;
+    if (!cbor_isa_bytestring(tag_str) || cbor_bytestring_length(tag_str) != 4) {
+      return StatusCode::kInvalidArgument;
+    }
+
+    hb_tag_t tag = hb_tag_from_string(reinterpret_cast<char*>(cbor_bytestring_handle(tag_str)),
+                                      4);
+
+    cbor_item_t* intervals = pairs->value;
+    if (!cbor_isa_array(tag_str)) {
+      return StatusCode::kInvalidArgument;
+    }
+
+    size_t intervals_length = cbor_array_size(intervals);
+    cbor_item_t** intervals_array = cbor_array_handle(intervals);
+    for (unsigned i = 0; i < intervals_length; i++) {
+      AxisInterval interval;
+      if ((sc = interval.Decode(*intervals_array[i], interval)) != StatusCode::kOk) {
+        return sc;
+      }
+
+      result._space[tag].push_back(std::move(interval));
+    }
+  }
+
+  return StatusCode::kOk;
+}
+
+StatusCode AxisSpace::Encode(cbor_item_unique_ptr& map_out) const {
+
+  cbor_item_unique_ptr map = make_cbor_map(_space.size());
+  StatusCode sc;
+
+  for (const auto& i : _space) {
+    char tag_str[4];
+    hb_tag_to_string(i.first, tag_str);
+    absl::string_view key(tag_str, 4);
+
+    cbor_item_unique_ptr cbor_key = wrap_cbor_item(CborUtils::EncodeBytes(key));
+    cbor_item_unique_ptr intervals_array = make_cbor_array(i.second.size());
+
+    for (const auto& interval : i.second) {
+
+      cbor_item_unique_ptr cbor_interval = empty_cbor_ptr();
+      if ((sc = interval.Encode(cbor_interval)) != StatusCode::kOk) {
+        return sc;
+      }
+
+      if (!cbor_array_push(intervals_array.get(), cbor_interval.get())) {
+        return StatusCode::kInternal;
+      }
+    }
+
+    bool ok = cbor_map_add(
+        map.get(), cbor_pair{.key = cbor_key.get(), .value = intervals_array.get()});
+
+    if (!ok) {
+      return StatusCode::kInternal;
+    }
+  }
+
+  map_out.swap(map);
+  return StatusCode::kOk;
+}
+
+AxisSpace& AxisSpace::operator=(AxisSpace&& other) noexcept {
+  _space = std::move(other._space);
+  return *this;
+}
+
+bool AxisSpace::operator==(const AxisSpace& other) const {
+  return _space == other._space;
+}
+
+bool AxisSpace::operator!=(const AxisSpace& other) const {
+  return !(*this == other);
+}
+
+}  // namespace patch_subset::cbor
---
+++ patch_subset/cbor/axis_space.h
@@ -0,0 +1,47 @@
+#ifndef PATCH_SUBSET_CBOR_AXIS_SPACE_H_
+#define PATCH_SUBSET_CBOR_AXIS_SPACE_H_
+
+
+#include "hb-subset.h"
+#include "absl/container/flat_hash_map.h"
+#include <optional>
+
+#include "cbor.h"
+#include "common/status.h"
+#include "patch_subset/cbor/axis_interval.h"
+#include "patch_subset/cbor/cbor_item_unique_ptr.h"
+
+namespace patch_subset::cbor {
+
+/*
+ * A range on a variation axis.
+ *
+ * See https://w3c.github.io/PFE/Overview.html#AxisSpace
+ */
+class AxisSpace {
+ private:
+  absl::flat_hash_map<hb_tag_t, std::vector<AxisInterval>> _space;
+
+ public:
+  AxisSpace() : _space() {}
+  AxisSpace(const AxisSpace& other) = default;
+  AxisSpace(AxisSpace&& other) noexcept
+      : _space(std::move(other._space)) {}
+  
+  bool Has(hb_tag_t tag) const;
+  void Clear(hb_tag_t tag);
+  void AddInterval(hb_tag_t tag, const AxisInterval& interval);
+  const std::vector<AxisInterval>& IntervalsFor(hb_tag_t tag) const;
+
+  static StatusCode Decode(const cbor_item_t& cbor_map, AxisSpace& out);
+  StatusCode Encode(cbor_item_unique_ptr& map_out) const;
+
+  
+  AxisSpace& operator=(AxisSpace&& other) noexcept;
+  bool operator==(const AxisSpace& other) const;
+  bool operator!=(const AxisSpace& other) const;
+};
+
+}  // namespace patch_subset::cbor
+
+#endif  // PATCH_SUBSET_CBOR_AXIS_SPACE_H_
