3aeb74453b3c3d017c788a480111e720cf5e0ceb
Garret Rieger <grieger@google.com>
2022-10-04 23:30:33 +0000

    Add more font diff tests.

--- util/brotli_font_diff_test.cc
+++ util/brotli_font_diff_test.cc
@@ -26,61 +26,118 @@ class BrotliFontDiffTest : public ::testing::Test {
   ~BrotliFontDiffTest() override {}
 
   void SetUp() override {
+    hb_blob_t* font_data =
+        hb_blob_create_from_file_or_fail("patch_subset/testdata/Roboto-Regular.ttf");
+    ASSERT_TRUE(font_data);
+    roboto = hb_face_create(font_data, 0);
+    hb_blob_destroy(font_data);
+
+    font_data =
+        hb_blob_create_from_file_or_fail("patch_subset/testdata/NotoSansJP-Regular.ttf");
+    ASSERT_TRUE(font_data);
+    noto_sans_jp = hb_face_create(font_data, 0);
+    hb_blob_destroy(font_data);
+
+    input = hb_subset_input_create_or_fail();
   }
+
+  void TearDown() override {
+    hb_face_destroy(roboto);
+    hb_subset_input_destroy(input);
+  }
+
+  void Check(const FontData& base,
+             const FontData& patch,
+             const FontData& derived) {
+    BrotliBinaryPatch patcher;
+    FontData patched;
+    EXPECT_EQ(StatusCode::kOk,
+              patcher.Patch(base, patch, &patched));
+
+    EXPECT_EQ(derived.str(), patched.str());
+  }
+
+  hb_face_t* roboto;
+  hb_face_t* noto_sans_jp;
+  hb_subset_input_t* input;
 };
 
-// TODO(garretrieger): more tests
-// - long loca.
-// - retain gids.
 TEST_F(BrotliFontDiffTest, Diff) {
-  hb_blob_t* font_data =
-      hb_blob_create_from_file_or_fail("patch_subset/testdata/Roboto-Regular.ttf");
-  ASSERT_TRUE(font_data);
-
-  hb_face_t* face = hb_face_create(font_data, 0);
-  hb_blob_destroy(font_data);
-
-  hb_subset_input_t* input = hb_subset_input_create_or_fail();
 
   hb_set_add_range(hb_subset_input_unicode_set(input), 0x41, 0x5A);
-  hb_subset_plan_t* base_plan = hb_subset_plan_create_or_fail(face, input);
+  hb_subset_plan_t* base_plan = hb_subset_plan_create_or_fail(roboto, input);
   hb_face_t* base_face = hb_subset_plan_execute_or_fail(base_plan);
-  hb_blob_t* base_data = hb_face_reference_blob(base_face);
+  FontData base = FontData::ToFontData(base_face);
   ASSERT_TRUE(base_plan);
 
   hb_set_add_range(hb_subset_input_unicode_set(input), 0x61, 0x7A);
-  hb_subset_plan_t* derived_plan = hb_subset_plan_create_or_fail(face, input);
+  hb_subset_plan_t* derived_plan = hb_subset_plan_create_or_fail(roboto, input);
   hb_face_t* derived_face = hb_subset_plan_execute_or_fail(derived_plan);
-  hb_blob_t* derived_data = hb_face_reference_blob(derived_face);
+  FontData derived = FontData::ToFontData(derived_face);
   ASSERT_TRUE(derived_plan);
-
-
-  FontData base;
-  base.copy(hb_blob_get_data(base_data, nullptr), hb_blob_get_length(base_data));
-  FontData derived;
-  derived.copy(hb_blob_get_data(derived_data, nullptr), hb_blob_get_length(derived_data));
 
   BrotliFontDiff differ;
   FontData patch;
   differ.Diff(base_plan, base_face, derived_plan, derived_face, &patch);
 
-  BrotliBinaryPatch patcher;
-  FontData patched;
-  EXPECT_EQ(StatusCode::kOk,
-            patcher.Patch(base, patch, &patched));
-
-  EXPECT_EQ(derived.str(), patched.str());
-
-  hb_subset_input_destroy(input);
   hb_subset_plan_destroy(base_plan);
   hb_subset_plan_destroy(derived_plan);
   hb_face_destroy(base_face);
   hb_face_destroy(derived_face);
-  hb_blob_destroy(base_data);
-  hb_blob_destroy(derived_data);
-  hb_face_destroy(face);
 }
 
+TEST_F(BrotliFontDiffTest, DiffRetainGids) {
+  hb_set_add_range(hb_subset_input_unicode_set(input), 0x41, 0x45);
+  hb_set_add_range(hb_subset_input_unicode_set(input), 0x57, 0x59);
+  hb_subset_input_set_flags(input, HB_SUBSET_FLAGS_RETAIN_GIDS);
+  hb_subset_plan_t* base_plan = hb_subset_plan_create_or_fail(roboto, input);
+  hb_face_t* base_face = hb_subset_plan_execute_or_fail(base_plan);
+  FontData base = FontData::ToFontData(base_face);
+  ASSERT_TRUE(base_plan);
+
+  hb_set_add(hb_subset_input_unicode_set(input), 0x47);
+  hb_subset_plan_t* derived_plan = hb_subset_plan_create_or_fail(roboto, input);
+  hb_face_t* derived_face = hb_subset_plan_execute_or_fail(derived_plan);
+  FontData derived = FontData::ToFontData(derived_face);
+  ASSERT_TRUE(derived_plan);
+
+  BrotliFontDiff differ;
+  FontData patch;
+  differ.Diff(base_plan, base_face, derived_plan, derived_face, &patch);
+
+  hb_subset_plan_destroy(base_plan);
+  hb_subset_plan_destroy(derived_plan);
+  hb_face_destroy(base_face);
+  hb_face_destroy(derived_face);
+}
+
+// TODO(garretrieger): diff where base is not a subset of derived.
+
+TEST_F(BrotliFontDiffTest, LongLoca) {
+
+  hb_set_add_range(hb_subset_input_glyph_set(input), 1000, 5000);
+  hb_set_add_range(hb_subset_input_glyph_set(input), 8000, 10000);
+  hb_subset_plan_t* base_plan = hb_subset_plan_create_or_fail(noto_sans_jp, input);
+  hb_face_t* base_face = hb_subset_plan_execute_or_fail(base_plan);
+  FontData base = FontData::ToFontData(base_face);
+  ASSERT_TRUE(base_plan);
+
+  hb_set_add_range(hb_subset_input_glyph_set(input),  500, 750);
+  hb_set_add_range(hb_subset_input_glyph_set(input), 11000, 11100);
+  hb_subset_plan_t* derived_plan = hb_subset_plan_create_or_fail(noto_sans_jp, input);
+  hb_face_t* derived_face = hb_subset_plan_execute_or_fail(derived_plan);
+  FontData derived = FontData::ToFontData(derived_face);
+  ASSERT_TRUE(derived_plan);
+
+  BrotliFontDiff differ;
+  FontData patch;
+  differ.Diff(base_plan, base_face, derived_plan, derived_face, &patch);
+
+  hb_subset_plan_destroy(base_plan);
+  hb_subset_plan_destroy(derived_plan);
+  hb_face_destroy(base_face);
+  hb_face_destroy(derived_face);
+}
 
 
 }  // namespace patch_subset
