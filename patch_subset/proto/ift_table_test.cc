196e30989a386903d8c90408cd15376200bffb3f
Garret Rieger <grieger@google.com>
2023-10-02 19:16:54 +0000

    Add 'IFTTable::AddToFont(...)'.

--- patch_subset/proto/ift_table_test.cc
+++ patch_subset/proto/ift_table_test.cc
@@ -1,12 +1,16 @@
 #include "patch_subset/proto/ift_table.h"
 
+#include <cstring>
+
 #include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
 #include "gtest/gtest.h"
 #include "patch_subset/hb_set_unique_ptr.h"
 #include "patch_subset/proto/IFT.pb.h"
 #include "patch_subset/sparse_bit_set.h"
 
 using absl::flat_hash_map;
+using absl::flat_hash_set;
 using patch_subset::SparseBitSet;
 
 namespace patch_subset::proto {
@@ -33,12 +37,63 @@ class IFTTableTest : public ::testing::Test {
     m->set_bias(0);
     m->set_codepoint_set(SparseBitSet::Encode(*set.get()));
     m->set_id(3);
+
+    hb_blob_t* blob =
+        hb_blob_create_from_file("patch_subset/testdata/Roboto-Regular.ab.ttf");
+    roboto_ab = hb_face_create(blob, 0);
+    hb_blob_destroy(blob);
   }
 
+  ~IFTTableTest() { hb_face_destroy(roboto_ab); }
+
+  hb_face_t* roboto_ab;
   IFT empty;
   IFT sample;
   IFT overlap_sample;
 };
+
+flat_hash_set<uint32_t> get_tags(hb_face_t* face) {
+  constexpr uint32_t max_tags = 64;
+  hb_tag_t table_tags[max_tags];
+  unsigned table_count = max_tags;
+
+  hb_face_get_table_tags(face, 0, &table_count, table_tags);
+
+  flat_hash_set<uint32_t> tag_set;
+  for (unsigned i = 0; i < table_count; i++) {
+    tag_set.insert(table_tags[i]);
+  }
+
+  return tag_set;
+}
+
+TEST_F(IFTTableTest, AddToFont) {
+  auto font = IFTTable::AddToFont(roboto_ab, sample);
+  ASSERT_TRUE(font.ok()) << font.status();
+
+  hb_face_t* face = font->reference_face();
+
+  hb_blob_t* blob = hb_face_reference_table(face, HB_TAG('I', 'F', 'T', ' '));
+
+  unsigned length = 0;
+  const char* data = hb_blob_get_data(blob, &length);
+
+  std::string expected = sample.SerializeAsString();
+  EXPECT_EQ(expected.size(), length);
+  if (expected.size() == length) {
+    EXPECT_EQ(memcmp(expected.data(), data, length), 0);
+  }
+
+  hb_blob_destroy(blob);
+
+  flat_hash_set<uint32_t> table_tags = get_tags(face);
+  flat_hash_set<uint32_t> expected_table_tags = get_tags(roboto_ab);
+  hb_face_destroy(face);
+
+  table_tags.erase(HB_TAG('I', 'F', 'T', ' '));
+
+  EXPECT_EQ(expected_table_tags, table_tags);
+}
 
 TEST_F(IFTTableTest, Empty) {
   auto table = IFTTable::FromProto(empty);
