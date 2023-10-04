cc3bd135b78a19f8f1fded2435c3d6cfa4a015e1
Garret Rieger <grieger@google.com>
2023-10-04 00:36:58 +0000

    Fix for IftbBinaryPatch.

--- ift/iftb_binary_patch.cc
+++ ift/iftb_binary_patch.cc
@@ -70,12 +70,20 @@ Status IftbBinaryPatch::Patch(const FontData& font_base,
   std::string font_data = font_base.string();
   sfnt sfnt;
   sfnt.setBuffer(font_data);
+  if (!sfnt.read()) {
+    return absl::InvalidArgumentError("Failed to read input font file.");
+  }
+
   hb_face_t* face = font_base.reference_face();
   unsigned num_glyphs = hb_face_get_glyph_count(face);
   hb_face_destroy(face);
 
   uint32_t new_length = merger.calcLayout(
       sfnt, num_glyphs, 0 /* TODO: add CFF charstrings offset */);
+  if (!new_length) {
+    return absl::InvalidArgumentError(
+        "Calculating layout before merge failed.");
+  }
   std::string new_font_data;
   new_font_data.reserve(new_length);
 
