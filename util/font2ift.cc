12b17ca75d05693e7b845f87c8bd5f8c2d197277
Garret Rieger <grieger@google.com>
2023-11-09 21:50:41 +0000

    Update font2ift to add the option to mix in IFTB patches.

--- util/font2ift.cc
+++ util/font2ift.cc
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <vector>
 
+#include "absl/container/btree_set.h"
 #include "absl/container/flat_hash_set.h"
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
@@ -26,6 +27,12 @@ ABSL_FLAG(std::string, output_font, "out.ttf",
 ABSL_FLAG(std::string, url_template, "patch$5$4$3$2$1.br",
           "Url template for patch files.");
 
+ABSL_FLAG(std::string, input_iftb_patch_template, "",
+          "Template used to locate existing iftb patches which should be "
+          "used in the output IFT font. If set the input codepoints files "
+          "are interpretted as patch indices instead of codepoints.");
+
+using absl::btree_set;
 using absl::flat_hash_set;
 using absl::Status;
 using absl::StatusOr;
@@ -33,6 +40,19 @@ using absl::StrCat;
 using ift::IFTClient;
 using ift::encoder::Encoder;
 using patch_subset::FontData;
+
+FontData load_iftb_patch(uint32_t index) {
+  std::string path = IFTClient::PatchToUrl(
+      absl::GetFlag(FLAGS_input_iftb_patch_template), index);
+  hb_blob_t* blob = hb_blob_create_from_file_or_fail(path.c_str());
+  if (!blob) {
+    fprintf(stderr, "failed to load file: %s\n", path.c_str());
+    exit(-1);
+  }
+
+  FontData result(blob);
+  return result;
+}
 
 hb_face_t* load_font(const char* filename) {
   hb_blob_t* blob = hb_blob_create_from_file_or_fail(filename);
@@ -89,6 +109,19 @@ Status write_file(const std::string& name, const FontData& data) {
   return absl::OkStatus();
 }
 
+void write_patch(const Encoder& encoder, uint32_t id, const FontData& patch) {
+  std::string output_path = absl::GetFlag(FLAGS_output_path);
+
+  std::string name = IFTClient::PatchToUrl(encoder.UrlTemplate(), id);
+  std::cerr << "  Writing patch: " << StrCat(output_path, "/", name)
+            << std::endl;
+  auto sc = write_file(StrCat(output_path, "/", name), patch);
+  if (!sc.ok()) {
+    std::cerr << sc.message() << std::endl;
+    exit(-1);
+  }
+}
+
 int write_output(const Encoder& encoder, const FontData& base_font) {
   std::string output_path = absl::GetFlag(FLAGS_output_path);
   std::string output_font = absl::GetFlag(FLAGS_output_font);
@@ -102,15 +135,7 @@ int write_output(const Encoder& encoder, const FontData& base_font) {
   }
 
   for (const auto& p : encoder.Patches()) {
-    std::string name = IFTClient::PatchToUrl(encoder.UrlTemplate(), p.first);
-
-    std::cerr << "Writing patch: " << StrCat(output_path, "/", name)
-              << std::endl;
-    auto sc = write_file(StrCat(output_path, "/", name), p.second);
-    if (!sc.ok()) {
-      std::cerr << sc.message() << std::endl;
-      return -1;
-    }
+    write_patch(encoder, p.first, p.second);
   }
 
   return 0;
@@ -139,7 +164,10 @@ int main(int argc, char** argv) {
     return -1;
   }
 
+  bool mixed_mode = !absl::GetFlag(FLAGS_input_iftb_patch_template).empty();
+
   Encoder encoder;
+  encoder.SetUrlTemplate(absl::GetFlag(FLAGS_url_template));
   {
     hb_face_t* input_font = load_font(args[1]);
     encoder.SetFace(input_font);
@@ -147,6 +175,39 @@ int main(int argc, char** argv) {
   }
 
   bool first = true;
+  if (mixed_mode) {
+    // Load and write out iftb patches
+    std::cout << ">> loading input iftb patches:" << std::endl;
+    for (size_t i = 2; i < args.size(); i++) {
+      auto s = load_unicodes_file(args[i]);
+      btree_set<uint32_t> ordered;
+      std::copy(s->begin(), s->end(), std::inserter(ordered, ordered.begin()));
+      if (!s.ok()) {
+        std::cerr << "Failed to load codepoint file (" << args[i]
+                  << "): " << s.status().message() << std::endl;
+        return -1;
+      }
+
+      for (uint32_t id : ordered) {
+        FontData iftb_patch = load_iftb_patch(id);
+        auto sc = encoder.AddExistingIftbPatch(id, iftb_patch);
+        if (!sc.ok()) {
+          std::cout << "Failed adding existing iftb patch: " << sc.message()
+                    << std::endl;
+          return -1;
+        }
+
+        if (!first) {
+          write_patch(encoder, id, iftb_patch);
+        }
+      }
+
+      first = false;
+    }
+  }
+
+  std::cout << ">> configuring encoder:" << std::endl;
+  first = true;
   for (size_t i = 2; i < args.size(); i++) {
     auto s = load_unicodes_file(args[i]);
     if (!s.ok()) {
@@ -154,19 +215,27 @@ int main(int argc, char** argv) {
       return -1;
     }
 
+    Status sc;
     if (first) {
-      auto sc = encoder.SetBaseSubset(*s);
+      sc = !mixed_mode ? encoder.SetBaseSubset(*s)
+                       : encoder.SetBaseSubsetFromIftbPatches(*s);
       if (!sc.ok()) {
         std::cerr << sc.message() << std::endl;
         return -1;
       }
       first = false;
-    } else {
+    } else if (!mixed_mode) {
       encoder.AddExtensionSubset(*s);
+    } else {
+      sc = encoder.AddExtensionSubsetOfIftbPatches(*s);
+      if (!sc.ok()) {
+        std::cerr << sc.message() << std::endl;
+        return -1;
+      }
     }
   }
 
-  encoder.SetUrlTemplate(absl::GetFlag(FLAGS_url_template));
+  std::cout << ">> encoding:" << std::endl;
   auto base_font = encoder.Encode();
   base_font = Encoder::EncodeWoff2(base_font->str(), false);
   if (!base_font.ok()) {
@@ -174,5 +243,6 @@ int main(int argc, char** argv) {
     return -1;
   }
 
+  std::cout << ">> generating output patches:" << std::endl;
   return write_output(encoder, *base_font);
 }
