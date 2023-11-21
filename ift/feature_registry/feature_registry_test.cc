965bd5c8f063e686ab79371dee8e8bfdb8e4da6a
Garret Rieger <grieger@google.com>
2023-11-21 01:12:58 +0000

    Add Index to Feature Tag mapping to the feature registry.

--- ift/feature_registry/feature_registry_test.cc
+++ ift/feature_registry/feature_registry_test.cc
@@ -1,9 +1,6 @@
 #include "ift/feature_registry/feature_registry.h"
 
-#include "absl/status/statusor.h"
 #include "gtest/gtest.h"
-
-using absl::StatusOr;
 
 namespace ift::feature_registry {
 
@@ -12,51 +9,46 @@ class FeatureRegistryTest : public ::testing::Test {
   FeatureRegistryTest() {}
 };
 
-TEST_F(FeatureRegistryTest, SpotCheckEntries) {
+TEST_F(FeatureRegistryTest, FeatureTagToIndex) {
   // Should match the feature registry in the specification found here:
   // https://w3c.github.io/IFT/Overview.html#feature-tag-list
   //
   // Spot check a few entries to make sure things seem to line up.
-  auto index = FeatureTagToIndex(HB_TAG('a', 'b', 'v', 'f'));
-  ASSERT_TRUE(index.ok()) << index.status();
-  ASSERT_EQ(*index, 1);
-
-  index = FeatureTagToIndex(HB_TAG('c', 'u', 'r', 's'));
-  ASSERT_TRUE(index.ok()) << index.status();
-  ASSERT_EQ(*index, 15);
-
-  index = FeatureTagToIndex(HB_TAG('a', 'a', 'l', 't'));
-  ASSERT_TRUE(index.ok()) << index.status();
-  ASSERT_EQ(*index, 66);
-
-  index = FeatureTagToIndex(HB_TAG('l', 'n', 'u', 'm'));
-  ASSERT_TRUE(index.ok()) << index.status();
-  ASSERT_EQ(*index, 90);
-
-  index = FeatureTagToIndex(HB_TAG('s', 's', '0', '1'));
-  ASSERT_TRUE(index.ok()) << index.status();
-  ASSERT_EQ(*index, 123);
-
-  index = FeatureTagToIndex(HB_TAG('s', 's', '1', '2'));
-  ASSERT_TRUE(index.ok()) << index.status();
-  ASSERT_EQ(*index, 134);
-
-  index = FeatureTagToIndex(HB_TAG('s', 's', '2', '0'));
-  ASSERT_TRUE(index.ok()) << index.status();
-  ASSERT_EQ(*index, 142);
-
-  index = FeatureTagToIndex(HB_TAG('c', 'v', '0', '1'));
-  ASSERT_TRUE(index.ok()) << index.status();
-  ASSERT_EQ(*index, 143);
-
-  index = FeatureTagToIndex(HB_TAG('c', 'v', '9', '9'));
-  ASSERT_TRUE(index.ok()) << index.status();
-  ASSERT_EQ(*index, 241);
+  ASSERT_EQ(FeatureTagToIndex(HB_TAG('a', 'b', 'v', 'f')), 1);
+  ASSERT_EQ(FeatureTagToIndex(HB_TAG('c', 'u', 'r', 's')), 15);
+  ASSERT_EQ(FeatureTagToIndex(HB_TAG('a', 'a', 'l', 't')), 66);
+  ASSERT_EQ(FeatureTagToIndex(HB_TAG('l', 'n', 'u', 'm')), 90);
+  ASSERT_EQ(FeatureTagToIndex(HB_TAG('s', 's', '0', '1')), 123);
+  ASSERT_EQ(FeatureTagToIndex(HB_TAG('s', 's', '1', '2')), 134);
+  ASSERT_EQ(FeatureTagToIndex(HB_TAG('s', 's', '2', '0')), 142);
+  ASSERT_EQ(FeatureTagToIndex(HB_TAG('c', 'v', '0', '1')), 143);
+  ASSERT_EQ(FeatureTagToIndex(HB_TAG('c', 'v', '9', '9')), 241);
 }
 
-TEST_F(FeatureRegistryTest, NotFound) {
-  auto index = FeatureTagToIndex(HB_TAG('x', 'x', 'x', 'x'));
-  ASSERT_TRUE(absl::IsNotFound(index.status())) << index.status();
+TEST_F(FeatureRegistryTest, IndexToFeatureTag) {
+  // Should match the feature registry in the specification found here:
+  // https://w3c.github.io/IFT/Overview.html#feature-tag-list
+  //
+  // Spot check a few entries to make sure things seem to line up.
+  ASSERT_EQ(IndexToFeatureTag(1), HB_TAG('a', 'b', 'v', 'f'));
+  ASSERT_EQ(IndexToFeatureTag(15), HB_TAG('c', 'u', 'r', 's'));
+  ASSERT_EQ(IndexToFeatureTag(66), HB_TAG('a', 'a', 'l', 't'));
+  ASSERT_EQ(IndexToFeatureTag(90), HB_TAG('l', 'n', 'u', 'm'));
+  ASSERT_EQ(IndexToFeatureTag(123), HB_TAG('s', 's', '0', '1'));
+  ASSERT_EQ(IndexToFeatureTag(134), HB_TAG('s', 's', '1', '2'));
+  ASSERT_EQ(IndexToFeatureTag(142), HB_TAG('s', 's', '2', '0'));
+  ASSERT_EQ(IndexToFeatureTag(143), HB_TAG('c', 'v', '0', '1'));
+  ASSERT_EQ(IndexToFeatureTag(241), HB_TAG('c', 'v', '9', '9'));
+}
+
+TEST_F(FeatureRegistryTest, FeatureTagToIndex_NotFound) {
+  ASSERT_EQ(FeatureTagToIndex(HB_TAG('x', 'x', 'x', 'x')),
+            HB_TAG('x', 'x', 'x', 'x'));
+}
+
+TEST_F(FeatureRegistryTest, IndexToFeatureTag_NotFound) {
+  ASSERT_EQ(IndexToFeatureTag(HB_TAG('x', 'x', 'x', 'x')),
+            HB_TAG('x', 'x', 'x', 'x'));
 }
 
 }  // namespace ift::feature_registry
