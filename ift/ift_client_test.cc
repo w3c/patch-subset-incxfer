e1236ec75d4e36e0f2cb99386ae5ae4bcaf613ea
Garret Rieger <grieger@google.com>
2023-12-08 01:17:33 +0000

    Update IFT client to expose patch urls instead of indices.
    This allows for proper handling of an extension specific url template. Also updated the IFTClient interface to only compute outstanding patches as part of Process(), this avoids running that multiple times as the desired subset definition is populated.

--- ift/ift_client_test.cc
+++ ift/ift_client_test.cc
@@ -34,6 +34,7 @@ static constexpr uint32_t kInvalidFont = 2;
 static constexpr uint32_t kLeaf = 3;
 static constexpr uint32_t kFontWithFeatures = 4;
 static constexpr uint32_t kFontWithDesignSpace = 5;
+static constexpr uint32_t kFontWithExtension = 6;
 
 static constexpr uint32_t kLiga = HB_TAG('l', 'i', 'g', 'a');
 static constexpr uint32_t kLigaNo = 30;
@@ -43,6 +44,8 @@ class IFTClientTest : public ::testing::Test {
   IFTClientTest() {
     // Simple Test Font
     IFT sample;
+    sample.set_url_template("0x$2$1");
+
     auto m = sample.add_subset_mapping();
     hb_set_unique_ptr set = make_hb_set(2, 7, 9);
     m->set_bias(23);
@@ -57,12 +60,20 @@ class IFTClientTest : public ::testing::Test {
     m->set_id_delta(32);
     m->set_patch_encoding(SHARED_BROTLI_ENCODING);
 
-    sample.set_url_template("https://localhost/patches/$2$1.patch");
+    // Extension table
+    IFT extension;
+    extension.set_url_template("0x$2$1.ext");
+    m = extension.add_subset_mapping();
+    set = make_hb_set(2, 60, 62);
+    m->set_bias(0);
+    m->set_codepoint_set(SparseBitSet::Encode(*set.get()));
+    m->set_id_delta(33);
+    m->set_patch_encoding(IFTB_ENCODING);
 
     // Complex Test Font
     IFT complex;
     complex.set_default_patch_encoding(IFTB_ENCODING);
-    complex.set_url_template("$1.patch");
+    complex.set_url_template("0x$2$1");
 
     m = complex.add_subset_mapping();
     set = make_hb_set(3, 4, 5, 6);
@@ -94,7 +105,7 @@ class IFTClientTest : public ::testing::Test {
     // Invalid Test Font
     IFT invalid;
     invalid.set_default_patch_encoding(IFTB_ENCODING);
-    invalid.set_url_template("$1.patch");
+    invalid.set_url_template("0x$2$1");
 
     m = invalid.add_subset_mapping();
     set = make_hb_set(3, 4, 5, 6);
@@ -152,8 +163,7 @@ class IFTClientTest : public ::testing::Test {
 
     // Simple Test Font
     IFTTable sample_with_design_space;
-    sample_with_design_space.SetUrlTemplate(
-        "https://localhost/patches/$2$1.patch");
+    sample_with_design_space.SetUrlTemplate("0x$2$1");
 
     PatchMap::Coverage coverage{10, 11, 12};
     sample_with_design_space.GetPatchMap().AddEntry(coverage, 0, IFTB_ENCODING);
@@ -178,6 +188,10 @@ class IFTClientTest : public ::testing::Test {
     auto font = IFTTable::AddToFont(face, sample);
     auto new_face = font->face();
     fonts[kSampleFont].set(new_face.get());
+
+    font = IFTTable::AddToFont(face, sample, &extension);
+    new_face = font->face();
+    fonts[kFontWithExtension].set(new_face.get());
 
     font = IFTTable::AddToFont(face, complex);
     new_face = font->face();
@@ -208,19 +222,20 @@ class IFTClientTest : public ::testing::Test {
     return result;
   }
 
-  FontData fonts[6];
+  FontData fonts[7];
 
   FontData iftb_font;
   FontData chunk1;
 };
 
 typedef flat_hash_set<uint32_t> uint_set;
+typedef flat_hash_set<std::string> string_set;
 
 struct PatchesNeededTestCase {
   PatchesNeededTestCase(
       uint32_t font_id_, uint_set codepoints_, uint_set features_,
       flat_hash_map<hb_tag_t, std::pair<float, float>> design_space_,
-      uint_set expected_)
+      string_set expected_)
       : font_id(font_id_),
         codepoints(codepoints_),
         features(features_),
@@ -231,7 +246,7 @@ struct PatchesNeededTestCase {
   uint_set codepoints;
   uint_set features;
   flat_hash_map<hb_tag_t, std::pair<float, float>> design_space;
-  uint_set expected;
+  string_set expected;
 };
 
 class IFTClientParameterizedTest
@@ -248,13 +263,11 @@ TEST_P(IFTClientParameterizedTest, PatchesNeeded) {
   ASSERT_TRUE(client.ok()) << client.status();
 
   if (!p.codepoints.empty()) {
-    auto s = client->AddDesiredCodepoints(p.codepoints);
-    ASSERT_TRUE(s.ok()) << s;
+    client->AddDesiredCodepoints(p.codepoints);
   }
 
   if (!p.features.empty()) {
-    auto s = client->AddDesiredFeatures(p.features);
-    ASSERT_TRUE(s.ok()) << s;
+    client->AddDesiredFeatures(p.features);
   }
 
   if (!p.design_space.empty()) {
@@ -264,28 +277,42 @@ TEST_P(IFTClientParameterizedTest, PatchesNeeded) {
     }
   }
 
+  auto s = client->Process();
+  ASSERT_TRUE(s.ok()) << s.status();
+  if (p.expected.empty()) {
+    ASSERT_EQ(IFTClient::READY, *s);
+  } else {
+    ASSERT_EQ(IFTClient::NEEDS_PATCHES, *s);
+  }
+
   ASSERT_EQ(client->PatchesNeeded(), p.expected);
 }
 
-// TODO(garretrieger): add tests with design spaces.
 INSTANTIATE_TEST_SUITE_P(
     TargetCodepoints, IFTClientParameterizedTest,
     testing::Values(
-        PatchesNeededTestCase(kSampleFont, {30}, {}, {}, {0x09}),
-        PatchesNeededTestCase(kSampleFont, {55, 57}, {}, {}, {0x2a}),
-        PatchesNeededTestCase(kSampleFont, {32, 56}, {}, {}, {0x09, 0x2a}),
-        PatchesNeededTestCase(kSampleFont, {32, 56}, {kLiga}, {}, {0x09, 0x2a}),
+        PatchesNeededTestCase(kSampleFont, {30}, {}, {}, {"0x09"}),
+
+        PatchesNeededTestCase(kSampleFont, {55, 57}, {}, {}, {"0x2a"}),
+        PatchesNeededTestCase(kSampleFont, {32, 56}, {}, {}, {"0x09", "0x2a"}),
+        PatchesNeededTestCase(kSampleFont, {32, 56}, {kLiga}, {},
+                              {"0x09", "0x2a"}),
         PatchesNeededTestCase(kSampleFont, {}, {}, {}, {}),
         PatchesNeededTestCase(kSampleFont, {112}, {}, {}, {}),
-        PatchesNeededTestCase(kSampleFont, {30, 112}, {}, {}, {0x09}),
+        PatchesNeededTestCase(kSampleFont, {30, 112}, {}, {}, {"0x09"}),
+
+        PatchesNeededTestCase(kFontWithExtension, {30}, {}, {}, {"0x09"}),
+        PatchesNeededTestCase(kFontWithExtension, {60}, {}, {}, {"0x22.ext"}),
+        PatchesNeededTestCase(kFontWithExtension, {30, 60}, {}, {},
+                              {"0x09", "0x22.ext"}),
 
         PatchesNeededTestCase(kFontWithFeatures, {21}, {kLiga}, {},
-                              {0x35, 0x40}),
+                              {"0x35", "0x40"}),
         PatchesNeededTestCase(kFontWithFeatures, {32, 56, 21}, {kLiga}, {},
-                              {0x09, 0x2a, 0x35, 0x40}),
+                              {"0x09", "0x2a", "0x35", "0x40"}),
         PatchesNeededTestCase(kFontWithFeatures, {32, 56}, {kLiga}, {},
-                              {0x09, 0x2a, 0x40}),
-        PatchesNeededTestCase(kFontWithFeatures, {100}, {kLiga}, {}, {0x40}),
+                              {"0x09", "0x2a", "0x40"}),
+        PatchesNeededTestCase(kFontWithFeatures, {100}, {kLiga}, {}, {"0x40"}),
 
         // dependent entry prioritization:
         // - goes the the largest intersection with ties broken by smaller entry
@@ -298,54 +325,62 @@ INSTANTIATE_TEST_SUITE_P(
         //   SBR  {70, 71, 72, 73}     0x37
         //   SBR  {80, 81, 82, 83, 84} 0x38
         PatchesNeededTestCase(kFontWithFeatures, {60, 70, 71}, {kLiga}, {},
-                              {0x37, 0x40}),
+                              {"0x37", "0x40"}),
         PatchesNeededTestCase(kFontWithFeatures, {60, 61, 70}, {kLiga}, {},
-                              {0x36, 0x40}),
+                              {"0x36", "0x40"}),
         PatchesNeededTestCase(kFontWithFeatures, {60, 61, 70, 71}, {kLiga}, {},
-                              {0x36, 0x40}),
+                              {"0x36", "0x40"}),
 
         // design space input has
         // IFTB {10, 11, 12}                  0
         // IFTB {10, 11, 12} wght [100, 400]  1
         // IFTB {}           wght [300, 700]  2
-        PatchesNeededTestCase(kFontWithDesignSpace, {11}, {}, {}, {0}),
+        PatchesNeededTestCase(kFontWithDesignSpace, {11}, {}, {}, {"0x00"}),
         PatchesNeededTestCase(kFontWithDesignSpace, {11}, {},
                               {{HB_TAG('w', 'g', 'h', 't'), {200, 200}}},
-                              {0, 1}),
+                              {"0x00", "0x01"}),
         PatchesNeededTestCase(kFontWithDesignSpace, {}, {},
                               {{HB_TAG('w', 'g', 'h', 't'), {200, 200}}}, {}),
         PatchesNeededTestCase(kFontWithDesignSpace, {}, {},
-                              {{HB_TAG('w', 'g', 'h', 't'), {500, 500}}}, {2}),
+                              {{HB_TAG('w', 'g', 'h', 't'), {500, 500}}},
+                              {"0x02"}),
         PatchesNeededTestCase(kFontWithDesignSpace, {11}, {},
                               {{HB_TAG('w', 'g', 'h', 't'), {350, 350}}},
-                              {0, 1, 2}),
+                              {"0x00", "0x01", "0x02"}),
         PatchesNeededTestCase(kFontWithDesignSpace, {}, {},
-                              {{HB_TAG('w', 'g', 'h', 't'), {350, 350}}}, {2}),
+                              {{HB_TAG('w', 'g', 'h', 't'), {350, 350}}},
+                              {"0x02"}),
         PatchesNeededTestCase(kFontWithDesignSpace, {11}, {},
-                              {{HB_TAG('w', 'g', 'h', 't'), {750, 750}}}, {0}),
+                              {{HB_TAG('w', 'g', 'h', 't'), {750, 750}}},
+                              {"0x00"}),
         PatchesNeededTestCase(kFontWithDesignSpace, {11}, {},
-                              {{HB_TAG('w', 'd', 't', 'h'), {350, 350}}}, {0}),
+                              {{HB_TAG('w', 'd', 't', 'h'), {350, 350}}},
+                              {"0x00"}),
 
         // Complex Font:
-        PatchesNeededTestCase(kComplexFont, {4, 6}, {}, {}, {1, 2}),
-        PatchesNeededTestCase(kComplexFont, {4, 11, 12}, {}, {}, {1, 3, 5}),
-        PatchesNeededTestCase(kComplexFont, {12}, {}, {}, {5}),
+        PatchesNeededTestCase(kComplexFont, {4, 6}, {}, {}, {"0x01", "0x02"}),
+        PatchesNeededTestCase(kComplexFont, {4, 11, 12}, {}, {},
+                              {"0x01", "0x03", "0x05"}),
+        PatchesNeededTestCase(kComplexFont, {12}, {}, {}, {"0x05"}),
 
-        PatchesNeededTestCase(kComplexFont, {5, 100}, {}, {}, {1}),
+        PatchesNeededTestCase(kComplexFont, {5, 100}, {}, {}, {"0x01"}),
         PatchesNeededTestCase(kComplexFont, {100}, {}, {}, {}),
 
-        PatchesNeededTestCase(kInvalidFont, {6}, {}, {}, {1}),
-        PatchesNeededTestCase(kInvalidFont, {6, 8}, {}, {}, {1}),
+        PatchesNeededTestCase(kInvalidFont, {6}, {}, {}, {"0x01"}),
+        PatchesNeededTestCase(kInvalidFont, {6, 8}, {}, {}, {"0x01"}),
 
         PatchesNeededTestCase(kLeaf, {30}, {}, {}, {})));
+
+// TODO(garretrieger): add test for merging desired design spaces.
 
 TEST_F(IFTClientTest, PatchUrls_InvalidRepeatedPatchIndices) {
   auto client = IFTClient::NewClient(std::move(fonts[kInvalidFont]));
   ASSERT_TRUE(client.ok()) << client.status();
 
   hb_set_unique_ptr codepoints = make_hb_set(1, 4);
-  auto s = client->AddDesiredCodepoints({4});
-  ASSERT_TRUE(absl::IsInternal(s)) << s;
+  client->AddDesiredCodepoints({4});
+  auto s = client->Process();
+  ASSERT_TRUE(absl::IsInternal(s.status())) << s.status();
 }
 
 TEST_F(IFTClientTest, ApplyPatches_IFTB) {
@@ -355,16 +390,42 @@ TEST_F(IFTClientTest, ApplyPatches_IFTB) {
   auto client = IFTClient::NewClient(std::move(iftb_font));
   ASSERT_TRUE(client.ok()) << client.status();
 
-  auto s = client->AddDesiredCodepoints({0xb7});
-  ASSERT_TRUE(s.ok()) << s;
-
-  uint_set expected = {1};
-  ASSERT_EQ(client->PatchesNeeded(), expected);
+  client->AddDesiredCodepoints({0xb7});
   auto state = client->Process();
   ASSERT_TRUE(state.ok()) << state.status();
   ASSERT_EQ(*state, IFTClient::NEEDS_PATCHES);
 
-  client->AddPatch(1, patches[0]);
+  string_set expected = {"./NotoSansJP-Regular.subset_iftb/chunk1.br"};
+  ASSERT_EQ(client->PatchesNeeded(), expected);
+
+  client->AddPatch("./NotoSansJP-Regular.subset_iftb/chunk1.br", patches[0]);
+
+  ASSERT_TRUE(client->PatchesNeeded().empty());
+
+  state = client->Process();
+  ASSERT_TRUE(state.ok()) << state.status();
+  ASSERT_EQ(*state, IFTClient::READY);
+}
+
+TEST_F(IFTClientTest, AddPatches_InvalidIgnored) {
+  std::vector<FontData> patches;
+  patches.emplace_back().shallow_copy(chunk1);
+
+  auto client = IFTClient::NewClient(std::move(iftb_font));
+  ASSERT_TRUE(client.ok()) << client.status();
+
+  client->AddDesiredCodepoints({0xb7});
+  auto state = client->Process();
+  ASSERT_TRUE(state.ok()) << state.status();
+  ASSERT_EQ(*state, IFTClient::NEEDS_PATCHES);
+
+  string_set expected = {"./NotoSansJP-Regular.subset_iftb/chunk1.br"};
+  ASSERT_EQ(client->PatchesNeeded(), expected);
+
+  client->AddPatch("./NotoSansJP-Regular.subset_iftb/chunk1.br", patches[0]);
+  // duplicates and invalid patches should be ignored.
+  client->AddPatch("./NotoSansJP-Regular.subset_iftb/chunk1.br", patches[0]);
+  client->AddPatch("invalid-patch.br", patches[0]);
 
   ASSERT_TRUE(client->PatchesNeeded().empty());
 
