6467311fcc9800441f13f9c344443546f44a86db
Garret Rieger <grieger@google.com>
2024-02-21 01:22:19 +0000

    Begin add serialization support for a custom binary encoding of the patch map (format 2).
    In the specification this will replace the protobuf based encoding.

---
+++ ift/proto/format_2_patch_map.h
@@ -0,0 +1,18 @@
+#ifndef IFT_PROTO_FORMAT_2_PATCH_MAP_H_
+#define IFT_PROTO_FORMAT_2_PATCH_MAP_H_
+
+#include "absl/status/statusor.h"
+#include "ift/proto/patch_map.h"
+
+namespace ift::proto {
+
+class Format2PatchMap {
+  static absl::Status Deserialize(absl::string_view data, PatchMap& out);
+  static absl::StatusOr<std::string> Serialize(const PatchMap& patch_map,
+                                               bool is_ext,
+                                               absl::string_view uri_template);
+};
+
+}  // namespace ift::proto
+
+#endif  // IFT_PROTO_FORMAT_2_PATCH_MAP_H_
