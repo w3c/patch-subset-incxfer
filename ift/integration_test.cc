c76b47970e4cd907cdef8658d494e3ba27ca754c
Garret Rieger <grieger@google.com>
2025-01-13 20:37:23 +0000

    Enable MixedMode_Complex integration test.

--- ift/integration_test.cc
+++ ift/integration_test.cc
@@ -682,10 +682,9 @@ TEST_F(IntegrationTest, MixedMode_LocaLenChange) {
       !FontHelper::GlyfData(extended_face.get(), gid_count_3 - 1)->empty());
 }
 
-/*
 TEST_F(IntegrationTest, MixedMode_Complex) {
   Encoder encoder;
-  auto sc = InitEncoderForIftb(encoder);
+  auto sc = InitEncoderForMixedMode(encoder);
   ASSERT_TRUE(sc.ok()) << sc;
 
   // target paritions: {{0}, {1, 2}, {3, 4}}
@@ -696,58 +695,34 @@ TEST_F(IntegrationTest, MixedMode_Complex) {
 
   auto encoded = encoder.Encode();
   ASSERT_TRUE(encoded.ok()) << encoded.status();
-
-  auto client = IFTClient::NewClient(std::move(*encoded));
-  ASSERT_TRUE(client.ok()) << client.status();
+  auto encoded_face = encoded->face();
 
   // Phase 1
-  client->AddDesiredCodepoints({chunk1_cp});
-  auto state = client->Process();
-  ASSERT_TRUE(state.ok()) << state.status();
-  ASSERT_EQ(*state, IFTClient::NEEDS_PATCHES);
-
-  auto patches = client->PatchesNeeded();
-  ASSERT_EQ(patches.size(), 2);  // 1 shared brotli and 1 iftb.
-
-  sc = AddPatches(*client, encoder);
-  ASSERT_TRUE(sc.ok()) << sc;
-
-  state = client->Process();
-  ASSERT_TRUE(state.ok()) << state.status();
-  ASSERT_EQ(*state, IFTClient::READY);
+  auto extended = Extend(encoder, *encoded, {chunk1_cp});
+  ASSERT_TRUE(extended.ok()) << extended.status();
+  auto extended_face = extended->face();
 
   // Phase 2
-  client->AddDesiredCodepoints({chunk3_cp});
-  state = client->Process();
-  ASSERT_TRUE(state.ok()) << state.status();
-  ASSERT_EQ(*state, IFTClient::NEEDS_PATCHES);
-
-  patches = client->PatchesNeeded();
-  ASSERT_EQ(patches.size(), 2);  // 1 shared brotli and 1 iftb.
-
-  sc = AddPatches(*client, encoder);
-  ASSERT_TRUE(sc.ok()) << sc;
-
-  state = client->Process();
-  ASSERT_TRUE(state.ok()) << state.status();
-  ASSERT_EQ(*state, IFTClient::READY);
+  extended = Extend(encoder, *extended, {chunk1_cp, chunk3_cp});
+  ASSERT_TRUE(extended.ok()) << extended.status();
+  extended_face = extended->face();
 
   // Check the results
-  auto codepoints = ToCodepointsSet(client->GetFontData());
+  auto codepoints = FontHelper::ToCodepointsSet(extended_face.get());
   ASSERT_TRUE(codepoints.contains(chunk0_cp));
   ASSERT_TRUE(codepoints.contains(chunk1_cp));
   ASSERT_TRUE(codepoints.contains(chunk2_cp));
   ASSERT_TRUE(codepoints.contains(chunk3_cp));
   ASSERT_TRUE(codepoints.contains(chunk4_cp));
 
-  auto face = client->GetFontData().face();
-  ASSERT_TRUE(!FontHelper::GlyfData(face.get(), chunk0_gid)->empty());
-  ASSERT_TRUE(!FontHelper::GlyfData(face.get(), chunk1_gid)->empty());
-  ASSERT_FALSE(!FontHelper::GlyfData(face.get(), chunk2_gid)->empty());
-  ASSERT_TRUE(!FontHelper::GlyfData(face.get(), chunk3_gid)->empty());
-  ASSERT_FALSE(!FontHelper::GlyfData(face.get(), chunk4_gid)->empty());
+  ASSERT_TRUE(!FontHelper::GlyfData(extended_face.get(), chunk0_gid)->empty());
+  ASSERT_TRUE(!FontHelper::GlyfData(extended_face.get(), chunk1_gid)->empty());
+  ASSERT_FALSE(!FontHelper::GlyfData(extended_face.get(), chunk2_gid)->empty());
+  ASSERT_TRUE(!FontHelper::GlyfData(extended_face.get(), chunk3_gid)->empty());
+  ASSERT_FALSE(!FontHelper::GlyfData(extended_face.get(), chunk4_gid)->empty());
 }
 
+/*
 TEST_F(IntegrationTest, MixedMode_SequentialDependentPatches) {
   Encoder encoder;
   auto sc = InitEncoderForIftb(encoder);
